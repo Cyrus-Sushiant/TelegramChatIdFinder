b094e5d6eec523f199514c9758a39d8d4f03c11e
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2022-09-14 22:18:01 +0430

    Add project files.

---
+++ TelegramChatIdFinder/Program.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot;
+
+var token = "";
+var botClient = new TelegramBotClient(token);
+
+var messages = await botClient.GetUpdatesAsync();
+
+foreach (var item in messages)
+{
+    Console.WriteLine($"ChatId: {item.Message.Chat.Id} => UserName: {item.Message.Chat.Username}");
+}
+
+Console.ReadLine();
